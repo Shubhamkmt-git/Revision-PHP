7d50acfba55cb9b6fee3ea04c95aae6f20cbe2a8
Shubham <shubhamkumawat325@gmail.com>
2026-04-24 00:10:17 +0530

    prinmting and statement using variables

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!-- statement printing "Hello, World!" to the browser. -->
+
 <?php
 echo "Hello, World!";
+?>
+
+<!-- statement printing a variable to the browser. -->
+
+<?php
+$greeting = "Hello, World!";
+echo $greeting;
 ?>
