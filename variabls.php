5cb396ba6deae269a1fd58fd1bb70153cf333d6d
Shubham <shubham332709@gmail.com>
2026-05-01 23:25:42 +0530

    variable decleration in php

--- variabls.php
+++ variabls.php
@@ -1,1 +1,18 @@
  <!-- variables in PHP -->
+  <?php
+$name = "Shubham";
+$age = 21;
+echo  "Name: " . $name . "<br>";
+echo  "Age: " . $age;
+// Rules of Variables
+// $ se start hona chahiye
+// Number se start nahi ho sakta  $1name
+// Spaces allowed nahi  $my name
+// Case-sensitive hai ($name ≠ $Name)
+
+
+$name = "Shubham";   // String
+$age = 21;          // Integer
+$price = 99.5;      // Float
+$isActive = true;   // Boolean
+?>
